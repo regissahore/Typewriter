90f01b3516bf3e23716db78d2fe562d5b49ec829
CyberZHG <CyberZHG@gmail.com>
2013-03-23 16:24:32 +0800

    Change the implement of message.

--- ZGO2/src/controller/message/message/messagemainwindowopen.cpp
+++
@@ -1,9 +0,0 @@
-#include "messagemainwindowopen.h"
-
-/**
- * 构造函数。
- */
-MessageMainWindowOpen::MessageMainWindowOpen() : Message()
-{
-    this->setName("MainWindow_Open");
-}
--- ZGO2/src/controller/message/message/messagemainwindowtryclose.cpp
+++
@@ -1,9 +0,0 @@
-#include "messagemainwindowtryclose.h"
-
-/**
- * 构造函数。
- */
-MessageMainWindowTryClose::MessageMainWindowTryClose() : Message()
-{
-    this->setName("MainWindow_TryClose");
-}
