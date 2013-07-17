828054a4b6062768e154d3b6774495c5b2ef77e8
CyberZHG <443442708@qq.com>
2013-07-17 21:35:00 +0800

    为项目设置默认字体。
    Signed-off-by: CyberZHG <443442708@qq.com>

--- ZGO2/src/main.cpp
+++ ZGO2/src/main.cpp
@@ -1,8 +1,22 @@
 #include <QDir>
+#include <QFont>
 #include <QString>
 #include <QTranslator>
 #include <QApplication>
+#include <QFontDatabase>
 #include "gomainwindow.h"
+
+void bindFont(QApplication &application)
+{
+    int fontId = QFontDatabase::addApplicationFont("font/default.ttf");
+    QStringList fontFamily = QFontDatabase::applicationFontFamilies(fontId);
+    if (!fontFamily.empty())
+    {
+        QFont font(fontFamily.at(0), 9);
+        font.setStyleStrategy(QFont::PreferAntialias);
+        application.setFont(font);
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -10,6 +24,7 @@ int main(int argc, char *argv[])
     QTranslator translator;
     translator.load("lang/Lang_CH.qm");
     application.installTranslator(&translator);
+    bindFont(application);
     GOMainWindow mainWindow;
     mainWindow.show();
     return application.exec();
