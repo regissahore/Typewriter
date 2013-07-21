72aafa6f1180117b18f4c857bd4eada50b93d4c9
CyberZHG <CyberZHG@gmail.com>
2013-07-21 17:10:14 +0800

    操作符6停工相关测试完成。

--- ZGO2/src/model/markov/gomarkovoperator6.cpp
+++ ZGO2/src/model/markov/gomarkovoperator6.cpp
@@ -110,16 +110,53 @@ double GOMarkovOperator6::calcTempOutputMarkovStatusNormal(double PS1, double PS
 
 void GOMarkovOperator6::calcOutputMarkovStatusCorrelate()
 {
+    GOMarkovStatus* prevStatus1 = this->getPrevMarkovStatus();
+    GOMarkovStatus* prevStatus2 = this->getPrevSubMarkovStatus();
+    double PS1 = prevStatus1->probabilityNormal();
+    double QS1 = 1.0 - PS1;
+    double PS2 = prevStatus2->probabilityNormal();
+    double QS2 = 1.0 - PS2;
+    double PC = this->markovStatus()->probabilityNormal();
+    double QC = 1.0 - PC;
+    double G1 = PS1 * PS2 * PC;
+    double G2 = QS1 * PS2 * PC + QS2 * PS1 * PC + QC * PS1 * PS2;
+    double PR = G1 / (G1 + G2);
+    double lambdaS1 = prevStatus1->frequencyBreakdown();
+    double lambdaS2 = prevStatus2->frequencyBreakdown();
+    double lambdaC = this->markovStatus()->frequencyBreakdown();
+    double lambdaR = lambdaS1 + lambdaS2 + lambdaC;
+    double muS1 = prevStatus1->frequencyRepair();
+    double muS2 = prevStatus2->frequencyRepair();
+    double muC = this->markovStatus()->frequencyRepair();
+    double muR = lambdaR / (lambdaS1 / muS1 + lambdaS2 / muS2 + lambdaC / muC);
+    this->initOutputMarkovStatus();
+    this->markovOutputStatus()->at(0)->setProbabilityNormal(PR);
+    this->markovOutputStatus()->at(0)->setFrequencyBreakdown(lambdaR);
+    this->markovOutputStatus()->at(0)->setFrequencyRepair(muR);
 }
 
 void GOMarkovOperator6::calcCommonOutputMarkovStatusCorrelate(double PR)
 {
-
+    GOMarkovStatus* prevStatus1 = this->getPrevMarkovStatus();
+    GOMarkovStatus* prevStatus2 = this->getPrevSubMarkovStatus();
+    double lambdaS1 = prevStatus1->frequencyBreakdown();
+    double lambdaS2 = prevStatus2->frequencyBreakdown();
+    double lambdaC = this->markovStatus()->frequencyBreakdown();
+    double lambdaR = lambdaS1 + lambdaS2 + lambdaC;
+    double muS1 = prevStatus1->frequencyRepair();
+    double muS2 = prevStatus2->frequencyRepair();
+    double muC = this->markovStatus()->frequencyRepair();
+    double muR = lambdaR / (lambdaS1 / muS1 + lambdaS2 / muS2 + lambdaC / muC);
+    this->initOutputMarkovStatus();
+    this->markovOutputStatus()->at(0)->setProbabilityNormal(PR);
+    this->markovOutputStatus()->at(0)->setFrequencyBreakdown(lambdaR);
+    this->markovOutputStatus()->at(0)->setFrequencyRepair(muR);
 }
 
 double GOMarkovOperator6::calcTempOutputMarkovStatusCorrelate(double PS1, double PS2)
 {
-
+    double PC = this->markovStatus()->probabilityNormal();
+    return PS1 * PS2 * PC;
 }
 
 void GOMarkovOperator6::save(QDomDocument &document, QDomElement &root)
