06deef691548deaf861b490989a6e5129a9afdbd
CyberZHG <CyberZHG@gmail.com>
2013-07-22 18:02:20 +0800

    操作符13B测试完成。

--- ZGO2/include/model/markov/gomarkovoperator13b.h
+++ ZGO2/include/model/markov/gomarkovoperator13b.h
@@ -1,12 +1,19 @@
 #ifndef GOMARKOVOPERATOR13B_H
 #define GOMARKOVOPERATOR13B_H
-
+/**
+ * 多路输入输出器"+"。
+ * @author ZHG <CyberZHG@gmail.com>
+ */
 #include "gomarkovoperator13.h"
 
 class GOMarkovOperator13B : public GOMarkovOperator13
 {
 public:
     GOMarkovOperator13B();
+    ~GOMarkovOperator13B();
+    void calcOutputMarkovStatus(double time);
+    void calcCommonOutputMarkovStatus(QVector<double> PR);
+    double calcTempOutputMarkovStatus(double time, QVector<double> input, QVector<double> subInput, int index);
 };
 
-#endif // GOMARKOVOPERATOR13B_H
+#endif // GOMARKOVOPERATOR13A_H
--- ZGO2/src/model/markov/gomarkovoperator13b.cpp
+++ ZGO2/src/model/markov/gomarkovoperator13b.cpp
@@ -1,5 +1,90 @@
 #include "gomarkovoperator13b.h"
+#include "goinput.h"
+#include "gooutput.h"
+#include "gomarkovstatus.h"
 
-GOMarkovOperator13B::GOMarkovOperator13B()
+GOMarkovOperator13B::GOMarkovOperator13B() : GOMarkovOperator13()
 {
+}
+
+GOMarkovOperator13B::~GOMarkovOperator13B()
+{
+    this->GOMarkovOperator13::~GOMarkovOperator13();
+}
+
+void GOMarkovOperator13B::calcOutputMarkovStatus(double time)
+{
+    Q_UNUSED(time);
+    QVector<double> PS;
+    QVector<double> muS;
+    for (int i = 0; i < this->input()->number(); ++i)
+    {
+        GOMarkovStatus *status = this->getPrevMarkovStatus(i);
+        PS.push_back(status->probabilityNormal());
+        muS.push_back(status->frequencyRepair());
+    }
+    for (int i = 0; i < this->output()->number(); ++i)
+    {
+        double PR = 0.0;
+        double muR = 0.0;
+        for (int j = 0; j < this->input()->number(); ++j)
+        {
+            if (this->relation()->at(j).at(i))
+            {
+                PR += PS[j];
+                muR += muS[j];
+            }
+        }
+        double QR = 1.0 - PR;
+        double lambdaR = muR * QR / PR;
+        this->markovOutputStatus()->at(i)->setProbabilityNormal(PR);
+        this->markovOutputStatus()->at(i)->setFrequencyBreakdown(lambdaR);
+        this->markovOutputStatus()->at(i)->setFrequencyRepair(muR);
+    }
+}
+
+void GOMarkovOperator13B::calcCommonOutputMarkovStatus(QVector<double> PR)
+{
+    QVector<double> muS;
+    for (int i = 0; i < this->input()->number(); ++i)
+    {
+        GOMarkovStatus *status = this->getPrevMarkovStatus(i);
+        muS.push_back(status->frequencyRepair());
+    }
+    for (int i = 0; i < this->output()->number(); ++i)
+    {
+        double muR = 0.0;
+        for (int j = 0; j < this->input()->number(); ++j)
+        {
+            if (this->relation()->at(j).at(i))
+            {
+                muR += muS[j];
+            }
+        }
+        double QR = 1.0 - PR[i];
+        double lambdaR = muR * QR / PR[i];
+        this->markovOutputStatus()->at(i)->setProbabilityNormal(PR[i]);
+        this->markovOutputStatus()->at(i)->setFrequencyBreakdown(lambdaR);
+        this->markovOutputStatus()->at(i)->setFrequencyRepair(muR);
+    }
+}
+
+double GOMarkovOperator13B::calcTempOutputMarkovStatus(double time, QVector<double> input, QVector<double> subInput, int index)
+{
+    Q_UNUSED(time);
+    Q_UNUSED(subInput);
+    QVector<double> PS;
+    for (int i = 0; i < this->input()->number(); ++i)
+    {
+        PS.push_back(input[i]);
+    }
+    double PR = 0.0;
+    for (int j = 0; j < this->input()->number(); ++j)
+    {
+        if (this->relation()->at(j).at(index))
+        {
+            PR += PS[j];
+        }
+    }
+    return PR;
 }
