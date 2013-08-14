179b5abd227f21ca2942ac5db06464fc6c7330e0
CyberZHG <CyberZHG@gmail.com>
2013-08-14 09:50:03 +0800

    共因失效显示独立故障率和共因故障率。

--- ZGO2/src/itemgomarkovcommoncause2.cpp
+++ ZGO2/src/itemgomarkovcommoncause2.cpp
@@ -37,10 +37,12 @@ void ItemGOMarkovCommonCause2::paint(QPainter *painter, const QStyleOptionGraphi
     painter->drawRoundedRect(0, 0, this->_end.x(), this->_end.y(), 10, 10);
     painter->drawText(QRectF(5, 5, 200, 100), Qt::AlignTop | Qt::AlignLeft, "C");
     painter->drawText(QRectF(5, 25, 200, 100), Qt::AlignTop | Qt::AlignLeft, QObject::tr("Total Breakdown: %1").arg(this->model()->breakdownTotal()));
-    painter->drawText(QRectF(5, 45, 200, 100), Qt::AlignTop | Qt::AlignLeft, QObject::tr("Belta: %1").arg(this->model()->belta()));
-    painter->drawText(QRectF(5, 65, 200, 100), Qt::AlignTop | Qt::AlignLeft, QObject::tr("Gamma C: %1").arg(this->model()->gammaC()));
-    painter->drawText(QRectF(5, 85, 200, 100), Qt::AlignTop | Qt::AlignLeft, QObject::tr("Operators: "));
-    int y = 105;
+    painter->drawText(QRectF(5, 45, 200, 100), Qt::AlignTop | Qt::AlignLeft, QObject::tr("Individual Breakdown: %1").arg(this->model()->breakdownIndividual()));
+    painter->drawText(QRectF(5, 65, 200, 100), Qt::AlignTop | Qt::AlignLeft, QObject::tr("Common Breakdown: %1").arg(this->model()->breakdownCommon()));
+    painter->drawText(QRectF(5, 85, 200, 100), Qt::AlignTop | Qt::AlignLeft, QObject::tr("Belta: %1").arg(this->model()->belta()));
+    painter->drawText(QRectF(5, 105, 200, 100), Qt::AlignTop | Qt::AlignLeft, QObject::tr("Gamma C: %1").arg(this->model()->gammaC()));
+    painter->drawText(QRectF(5, 125, 200, 100), Qt::AlignTop | Qt::AlignLeft, QObject::tr("Operators: "));
+    int y = 145;
     for (int i = 0; i < this->_operatorItems->size(); ++i)
     {
         painter->drawText(QRectF(5, y, 200, 100), Qt::AlignTop | Qt::AlignLeft, GOMarkovOperatorFactory::typeName(this->_operatorItems->at(i)->model()->TypedItem::type()) + QString(" - %1").arg(this->_operatorItems->at(i)->model()->id()));
@@ -73,7 +75,7 @@ void ItemGOMarkovCommonCause2::bindOperators(QList<ItemGOMarkovOperator*> &opera
 
 void ItemGOMarkovCommonCause2::updateBoundary()
 {
-    this->_end.setY(105 + 20 * this->_operatorItems->size() + 5);
+    this->_end.setY(145 + 20 * this->_operatorItems->size() + 5);
 }
 
 QVector<ItemGOMarkovOperator*>* ItemGOMarkovCommonCause2::operatorItems() const
