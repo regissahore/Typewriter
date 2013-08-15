7b4590af8b3bbdae93484fa3bb5e8897a81a2a4a
CyberZHG <CyberZHG@gmail.com>
2013-08-15 10:45:03 +0800

    修改向量化后22B的计算。

--- ZGO2/src/gomarkovoperator22b.cpp
+++ ZGO2/src/gomarkovoperator22b.cpp
@@ -22,25 +22,24 @@ GOMarkovOperator22B::~GOMarkovOperator22B()
 void GOMarkovOperator22B::calcOutputMarkovStatus(double time)
 {
     GOMarkovStatus *status1 = this->getPrevMarkovStatus(0);
+    GOMarkovStatus *status2 = this->getPrevSubMarkovStatus();
     GOMarkovStatus *status3 = this->getPrevMarkovStatus(1);
-    GOMarkovOperator *prevOperator = this->getPrevSubOperator();
     DoubleVector PS1 = status1->probabilityNormal();
+    DoubleVector PS2 = status2->probabilityNormal();
     DoubleVector PS3 = status3->probabilityNormal();
     DoubleVector lambdaS1 = status1->frequencyBreakdown();
+    DoubleVector lambdaS2 = status2->frequencyBreakdown();
     DoubleVector lambdaS3 = status3->frequencyBreakdown();
     DoubleVector PC1 = this->markovStatus()->probabilityNormal();
     DoubleVector lambdaC1 = this->markovStatus()->frequencyBreakdown();
     for (int i = 0; i < this->output()->number(); ++i)
     {
-        GOMarkovStatus *status2 = prevOperator->markovOutputStatus()->at(i);
-        DoubleVector PS2 = status2->probabilityNormal();
-        DoubleVector lambdaS2 = status2->frequencyBreakdown();
         double lambda = this->lambda2()->at(i);
         double mu = this->mu2()->at(i);
         DoubleVector PC2 = mu / (lambda + mu) * (1 + lambda / mu * exp(-(lambda + mu) * time));
-        DoubleVector PR = PS1 * PS3 * PC1 * PS2 + PS1 * PS3 * PC2 * PS2;
+        DoubleVector PR = PS1 * PS3 * PC1 * PS2.getValue(i) + PS1 * PS3 * PC2 * PS2.getValue(i);
         DoubleVector QR = 1.0 - PR;
-        DoubleVector lambdaR = lambdaS1 + lambdaS2 + lambdaS3 + lambdaC1 + lambda;
+        DoubleVector lambdaR = lambdaS1 + lambdaS2.getValue(i) + lambdaS3 + lambdaC1 + lambda;
         DoubleVector muR = lambdaR * PR / QR;
         this->markovOutputStatus()->at(i)->setProbabilityNormal(PR);
         this->markovOutputStatus()->at(i)->setFrequencyBreakdown(lambdaR);
@@ -51,18 +50,17 @@ void GOMarkovOperator22B::calcOutputMarkovStatus(double time)
 void GOMarkovOperator22B::calcCommonOutputMarkovStatus(QVector<DoubleVector> PR)
 {
     GOMarkovStatus *status1 = this->getPrevMarkovStatus(0);
+    GOMarkovStatus *status2 = this->getPrevSubMarkovStatus();
     GOMarkovStatus *status3 = this->getPrevMarkovStatus(1);
-    GOMarkovOperator *prevOperator = this->getPrevSubOperator();
     DoubleVector lambdaS1 = status1->frequencyBreakdown();
+    DoubleVector lambdaS2 = status2->frequencyBreakdown();
     DoubleVector lambdaS3 = status3->frequencyBreakdown();
     DoubleVector lambdaC1 = this->markovStatus()->frequencyBreakdown();
     for (int i = 0; i < this->output()->number(); ++i)
     {
-        GOMarkovStatus *status2 = prevOperator->markovOutputStatus()->at(i);
-        DoubleVector lambdaS2 = status2->frequencyBreakdown();
         double lambda = this->lambda2()->at(i);
         DoubleVector QR = 1.0 - PR[i];
-        DoubleVector lambdaR = lambdaS1 + lambdaS2 + lambdaS3 + lambdaC1 + lambda;
+        DoubleVector lambdaR = lambdaS1 + lambdaS2.getValue(0) + lambdaS3 + lambdaC1 + lambda;
         DoubleVector muR = lambdaR * PR[i] / QR;
         this->markovOutputStatus()->at(i)->setProbabilityNormal(PR[i]);
         this->markovOutputStatus()->at(i)->setFrequencyBreakdown(lambdaR);
@@ -72,13 +70,14 @@ void GOMarkovOperator22B::calcCommonOutputMarkovStatus(QVector<DoubleVector> PR)
 
 DoubleVector GOMarkovOperator22B::calcTempOutputMarkovStatus(double time, QVector<DoubleVector> input, QVector<DoubleVector> subInput, int index)
 {
+    Q_UNUSED(index);
     DoubleVector PS1 = input[0];
+    DoubleVector PS2 = subInput[0];
     DoubleVector PS3 = input[2];
     DoubleVector PC1 = this->markovStatus()->probabilityNormal();
-    DoubleVector PS2 = subInput[index];
     double lambda = this->lambda2()->at(index);
     double mu = this->mu2()->at(index);
     DoubleVector PC2 = mu / (lambda + mu) * (1 + lambda / mu * exp(-(lambda + mu) * time));
-    DoubleVector PR = PS1 * PS3 * PC1 * PS2 + PS1 * PS3 * PC2 * PS2;
+    DoubleVector PR = PS1 * PS3 * PC1 * PS2.getValue(index) + PS1 * PS3 * PC2 * PS2.getValue(index);
     return PR;
 }
