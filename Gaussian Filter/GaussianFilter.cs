cc19b883a0e7e8651d5837a206d328bf3b0c8b26
cyberzhg <cyberzhg@gmail.com>
2013-12-30 13:32:32 +0800

    Gaussian filter on RGB image.
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

---
+++ Gaussian Filter/GaussianFilter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+
+namespace Gaussian
+{
+    class GaussianFilter
+    {
+        public static void filter(Bitmap origin, out Bitmap delt, int rowNum, int colNum, double sigma)
+        {
+            GaussianMask mask = new GaussianMask(rowNum, colNum, sigma);
+            delt = new Bitmap(origin.Width, origin.Height);
+            int midRow = rowNum >> 1;
+            int midCol = colNum >> 1;
+            for (int r = 0; r < origin.Height; ++r)
+            {
+                for (int c = 0; c < origin.Width; ++c)
+                {
+                    double red = 0.0;
+                    double green = 0.0;
+                    double blue = 0.0;
+                    for (int i = 0; i < rowNum; ++i)
+                    {
+                        for (int j = 0; j < colNum; ++j)
+                        {
+                            int shiftRow = (r + i - midRow + origin.Height) % origin.Height;
+                            int shiftCol = (c + j - midCol + origin.Width) % origin.Width;
+                            Color color = origin.GetPixel(shiftCol, shiftRow);
+                            red += color.R * mask.mask[i, j];
+                            green += color.G * mask.mask[i, j];
+                            blue += color.B * mask.mask[i, j];
+                        }
+                    }
+                    delt.SetPixel(c, r, Color.FromArgb((int)red, (int)green, (int)blue));
+                }
+            }
+        }
+    }
+}
