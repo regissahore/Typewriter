5b7abb3cfaf32b42905fa4cdc24c4726c65e9b90
CyberZHG <CyberZHG@gmail.com>
2014-01-19 22:09:01 +0800

    本体程序。
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- Guide - Game Development Guide/Guide_001/test.py
+++ Guide - Game Development Guide/Guide_001/test.py
@@ -2,17 +2,19 @@
 # 这是用来验证是否正确安装所需库的程序。
 # 不需要理会其中的内容。
 # 如果可以运行看到一个黑色的窗口库安装正常。
+import sys
 import pygame
 from pygame.locals import *
 
 print pygame.ver
 
 pygame.init()
-screen = pygame.display.set_mode((640, 480), 0, 32)
-pygame.display.set_caption('Test');
+pygame.display.set_mode((640, 480), 0, 32)
+pygame.display.set_caption('Test')
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.locals.QUIT:
-            exit()
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
     pygame.display.update()
