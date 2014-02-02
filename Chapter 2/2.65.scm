d6eb74aa4d34e3e3936824b6a30a19d0dad029e3
CyberZHG <CyberZHG@gmail.com>
2014-02-02 15:42:04 +0800

    2.63 - 2.66
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

---
+++ Chapter 2/2.65.scm
@@ -0,0 +1,7 @@
+(load "2.64.scm")
+(define (union-tree tree1 tree2)
+  (list->tree (union-set (tree->list-2 tree1)
+                         (tree->list-2 tree2))))
+(define (intersection-tree tree1 tree2)
+  (list->tree (intersection-set (tree->list-2 tree1)
+                                (tree->list-2 tree2))))
