53b609162f581987899faff0fcb4bfa9fe045869
cyberzhg <cyberzhg@gmail.com>
2014-06-19 12:28:45 +0800

    Voronoi
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- Gist/conn.m
+++ Gist/conn.m
@@ -1,13 +1,16 @@
-for i = 0 : 295
+for i = 0 : 260
     name = sprintf('grid%d.jpg', i);
     im = imread(name);
     [I, map] = rgb2ind(im, 20);
     if i == 0
-        imwrite(I, map, 'res.gif', 'gif', 'Loopcount', inf, 'DelayTime', 0.01);
+        imwrite(I, map, 'res.gif', 'gif', 'Loopcount', inf, 'DelayTime', 0.1);
     else
-        imwrite(I, map, 'res.gif', 'gif', 'WriteMode', 'append', 'DelayTime', 0.01);
+        imwrite(I, map, 'res.gif', 'gif', 'WriteMode', 'append', 'DelayTime', 0.1);
     end
 end
-for i = 0 : 50
-    imwrite(I, map, 'res.gif', 'gif', 'WriteMode', 'append', 'DelayTime', 0.01);
+for i = 259 : -1 : 1
+    name = sprintf('grid%d.jpg', i);
+    im = imread(name);
+    [I, map] = rgb2ind(im, 20);
+    imwrite(I, map, 'res.gif', 'gif', 'WriteMode', 'append', 'DelayTime', 0.1);
 end
--- Gist/grid.py
+++ Gist/grid.py
@@ -1,60 +1,56 @@
-from PIL import Image, ImageDraw
+from random import randint, uniform
 from math import sqrt
+from PIL import Image, ImageDraw
 
-GRID_SIZE = 32
-RECT_WIDTH = 16
-
-grid = [[' ' for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
-isNew = False
-cur = 0
-k = 0
-cnt = 0
-image = Image.new('RGB', (GRID_SIZE * RECT_WIDTH, GRID_SIZE * RECT_WIDTH), color=(255, 255, 255))
-draw = ImageDraw.Draw(image)
-while True:
-    if not isNew:
-        cx = int(k * (sqrt(5.0) + 1.0) * 0.5);
-        cy = cx + k;
-        if cy >= GRID_SIZE:
-            break
-        k += 1
-        grid[cx][cy] = grid[cy][cx] = 'P'
-        draw.rectangle((cx * RECT_WIDTH, cy * RECT_WIDTH, (cx + 1) * RECT_WIDTH, (cy + 1) * RECT_WIDTH), fill = (0, 0, 0))
-        draw.rectangle((cy * RECT_WIDTH, cx * RECT_WIDTH, (cy + 1) * RECT_WIDTH, (cx + 1) * RECT_WIDTH), fill = (0, 0, 0))
-        cur = 1
-        isNew = True
-        if k % 2 == 0:
-            color = (200, 200, 200)
-        else:
-            color = (150, 150, 150)
-        continue
-    isNew = False
-    if cx + cur < GRID_SIZE:
-        grid[cx + cur][cy] = 'N'
-        grid[cy][cx + cur] = 'N'
-        x = cx + cur
-        y = cy
-        draw.rectangle((x * RECT_WIDTH, y * RECT_WIDTH, (x + 1) * RECT_WIDTH, (y + 1) * RECT_WIDTH), fill = color)
-        draw.rectangle((y * RECT_WIDTH, x * RECT_WIDTH, (y + 1) * RECT_WIDTH, (x + 1) * RECT_WIDTH), fill = color)
-        isNew = True
-        if cy + cur < GRID_SIZE:
-            grid[cx + cur][cy + cur] = 'N'
-            grid[cy + cur][cx + cur] = 'N'
-            x = cx + cur
-            y = cy + cur
-            draw.rectangle((x * RECT_WIDTH, y * RECT_WIDTH, (x + 1) * RECT_WIDTH, (y + 1) * RECT_WIDTH), fill = color)
-            draw.rectangle((y * RECT_WIDTH, x * RECT_WIDTH, (y + 1) * RECT_WIDTH, (x + 1) * RECT_WIDTH), fill = color)
-    if cy + cur < GRID_SIZE:
-        grid[cx][cy + cur] = 'N'
-        grid[cy + cur][cx] = 'N'
-        x = cx
-        y = cy + cur
-        draw.rectangle((x * RECT_WIDTH, y * RECT_WIDTH, (x + 1) * RECT_WIDTH, (y + 1) * RECT_WIDTH), fill = color)
-        draw.rectangle((y * RECT_WIDTH, x * RECT_WIDTH, (y + 1) * RECT_WIDTH, (x + 1) * RECT_WIDTH), fill = color)
-        isNew = True
-    cur += 1
-    image.save('grid' + str(cnt) + '.jpg', 'JPEG')
-    cnt += 1
+def get_dist1(x1, y1, x2, y2):
+    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
     
-        
-        
+def get_dist2(x1, y1, x2, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
+    
+def get_dist3(x1, y1, x2, y2):
+    return max(abs(x1 - x2), abs(y1 - y2))
+    
+def get_dist(x1, y1, x2, y2):
+    return get_dist3(x1, y1, x2, y2)
+
+center = [(randint(0, 255), randint(0, 255)) for _ in range(7)]
+color = [(237, 28, 36), (255, 127, 39), (34, 177, 76), (63, 72, 204), (163, 73, 164),
+         (0, 162, 232), (185, 122, 87)]
+pixel = [[center[i] for j in range(5000)] for i in range(7)]
+for k in range(1000):
+    print k
+    image = Image.new('RGB', (256, 256), color=(255, 255, 255))
+    draw = ImageDraw.Draw(image)
+    for i in range(7):
+        cx, cy = center[i]
+        draw.rectangle((cx - 2, cy - 2, cx + 3, cy + 3), fill=color[i])
+        for j in range(5000):
+            x, y = pixel[i][j]
+            origin_dist = get_dist(x, y, cx, cy)
+            x += randint(-5, 5)
+            y += randint(-5, 5)
+            dist = get_dist(x, y, cx, cy)
+            flag = True
+            if x < 0 or x >= 256 or y < 0 or y >= 256:
+                flag = False
+            if flag:
+                for ii in range(7):
+                    if i != ii:
+                        tx, ty = center[ii]
+                        temp_dist = get_dist(x, y, tx, ty)
+                        if temp_dist < dist:
+                            flag = False
+                            break
+            if flag:
+                if dist >= origin_dist:
+                    if randint(1, 100) > 45:
+                        pixel[i][j] = (x, y)
+                        origin_dist = dist
+                else:
+                    if randint(1, 100) > 55:
+                        pixel[i][j] = (x, y)
+            x, y = pixel[i][j]
+            draw.rectangle((x, y, x + 1, y + 1), fill=color[i])
+    image.save('grid' + str(k) + '.jpg', 'JPEG')
+    
