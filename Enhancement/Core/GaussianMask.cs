2365eef7eb1e7db884e8482f72a23ee1c7a63d21
cyberzhg <cyberzhg@gmail.com>
2014-06-20 10:03:36 +0800

    Add existed file.
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

---
+++ Enhancement/Core/GaussianMask.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train
+{
+    class GaussianMask
+    {
+        public double[,] mask;
+
+        public GaussianMask(int rowNum, int colNum, double sigma)
+        {
+            int mr = rowNum >> 1;
+            int mc = colNum >> 1;
+            this.mask = new double[rowNum, colNum];
+            double constant1 = -1.0 / (2.0 * sigma * sigma);
+            double constant2 = 1.0 / (2 * Math.PI * sigma * sigma);
+            double sum = 0.0;
+            for (int r = 0; r < rowNum; ++r)
+            {
+                for (int c = 0; c < colNum; ++c)
+                {
+                    int dr = r - mr;
+                    int dc = c - mc;
+                    this.mask[r, c] = Math.Exp((dr * dr + dc * dc) * constant1) * constant2;
+                    sum += this.mask[r, c];
+                }
+            }
+            for (int r = 0; r < rowNum; ++r)
+            {
+                for (int c = 0; c < colNum; ++c)
+                {
+                    this.mask[r, c] /= sum;
+                }
+            }
+        }
+
+        public int rowNum()
+        {
+            return mask.GetLength(0);
+        }
+
+        public int colNum()
+        {
+            return mask.GetLength(1);
+        }
+    }
+}
