cff3af6644098fc1d42f21965ab951ece22f051f
cyberzhg <cyberzhg@gmail.com>
2014-06-22 13:11:54 +0800

    Add game logic.
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- Rodeo/redeo.coffee
+++ Rodeo/redeo.coffee
@@ -1,11 +1,15 @@
 class Game
     constructor: (@gameDiv) ->
+        @stepx = [0, 1, 0, -1]
+        @stepy = [1, 0, -1, 0]
+        
     init: =>
-        @depth = 0
-        @score = []
+        @depth = 1
+        @scores = []
         @total = 0
         @initBoard()
         @initGame()
+        
     initBoard: =>
         @gameDiv.innerHTML = ''
         @board = []
@@ -21,34 +25,176 @@ class Game
                 @board[i][j].div.onclick = do (i, j) =>
                     => @click i, j
             @gameDiv.appendChild gameRow
+            
     initGame: =>
+        @turn = 1
+        @step = 0
+        @win = false
+        @lose = false
         for i in [0..4]
             for j in [0..4]
                 @setNum i, j, 0
                 @setBelong i, j, 0
         for i in [0..4]
             for j in [-1..2]
-                num = j
-                if j == 0
-                    num = 1
+                num = if j == 0 then 1 else j 
                 while true
                     x = Math.floor Math.random() * 5
                     y = Math.floor Math.random() * 5
                     if @board[x][y].num == 0
                         @setNum x, y, num
                         break
+                        
     setNum: (x, y, num) =>
         @board[x][y].num = num
-        switch num
+        @updateStyle x, y
+        
+    setBelong: (x, y, belong) =>
+        @board[x][y].belong = belong
+        @updateStyle x, y
+        
+    setHighLight: (x, y, highLight) =>
+        @board[x][y].highLight = highLight
+        @updateStyle x, y
+        
+    updateStyle: (x, y) =>
+        switch @board[x][y].num
             when -1 then text = '-'
             when 0 then text = ''
             when 1 then text = '+'
             when 2 then text = '++'
         @board[x][y].div.innerText = text
-    setBelong: (x, y, belong) =>
-        @board[x][y].belong = belong
+        switch @board[x][y].belong
+            when 0 then className = 'game_piece'
+            when 1 then className = 'game_piece' + (if @board[x][y].highLight then ' region1' else ' belong1')
+            when 2 then className = 'game_piece' + (if @board[x][y].highLight then ' region2' else ' belong2')
+        @board[x][y].div.className = className
+        
+    getRegionScore: (x, y) =>
+        score = @board[x][y].num
+        @board[x][y].visit = true
+        for k in [0..3]
+            tx = x + @stepx[k]
+            ty = y + @stepy[k]
+            if tx >= 0 and tx <= 4
+                if ty >= 0 and ty <= 4
+                    if not @board[tx][ty].visit
+                        if @board[tx][ty].belong == @board[x][y].belong
+                            score += @getRegionScore tx, ty
+        return score
+        
+    getScore: =>
+        for i in [0..4]
+            for j in [0..4]
+                @board[i][j].visit = false
+        score1 = 0
+        score2 = 0
+        for i in [0..4]
+            for j in [0..4]
+                if not @board[i][j].visit
+                    score = @getRegionScore i, j
+                    if @board[i][j].belong == 1
+                        score1 = score if score > score1
+                    else if @board[i][j].belong == 2
+                        score2 = score if score > score2
+        score1 - score2
+        
+    maxSearch: (beta, depth, num) =>
+        if depth == 0 or num == 25
+            return @getScore()
+        alpha = -1e10
+        for x in [0..4]
+            for y in [0..4]
+                if @board[x][y].belong == 0
+                    @board[x][y].belong = 1
+                    score = @minSearch alpha, depth - 1, num + 1
+                    @board[x][y].belong = 0
+                    alpha = score if score > alpha
+                    return alpha if alpha >= beta
+        return alpha
+        
+    minSearch: (alpha, depth, num) =>
+        if depth == 0 or num == 25
+            return @getScore()
+        beta = 1e10
+        for x in [0..4]
+            for y in [0..4]
+                if @board[x][y].belong == 0
+                    @board[x][y].belong = 2
+                    score = @maxSearch alpha, depth - 1, num + 1
+                    @board[x][y].belong = 0
+                    if score < beta
+                        beta = score 
+                        if num == @step
+                            @nextx = x
+                            @nexty = y
+                    return beta if alpha >= beta
+        return beta
+        
     click: (x, y) =>
-        console.log x, y
+        if not @win and not @lose
+            if @board[x][y].belong == 0
+                @setBelong x, y, 1
+                @board[x][y].belong = 1
+                @step += 1
+                if @step == 25
+                    if @getScore() > 0 then @doWin() else @doLose()
+                else
+                    @minSearch -1e10, @depth, @step
+                    @setBelong @nextx, @nexty, 2
+                    @step += 1
+                @highLight()
+                
+    highLightRegion: (x, y) =>
+        @board[x][y].highLight = true
+        for k in [0..3]
+            tx = x + @stepx[k]
+            ty = y + @stepy[k]
+            if tx >= 0 and tx <= 4
+                if ty >= 0 and ty <= 4
+                    if not @board[tx][ty].highLight
+                        if @board[tx][ty].belong == @board[x][y].belong
+                            @highLightRegion tx, ty 
+                
+    highLight: () =>
+        for i in [0..4]
+            for j in [0..4]
+                @board[i][j].visit = false
+                @setHighLight i, j, false
+        return if @step == 0
+        score1 = 0
+        score2 = 0
+        for i in [0..4]
+            for j in [0..4]
+                if not @board[i][j].visit
+                    score = @getRegionScore i, j
+                    if @board[i][j].belong == 1
+                        if score > score1
+                            score1 = score
+                            x1 = i
+                            y1 = j
+                    else if @board[i][j].belong == 2
+                        if score > score2
+                            x2 = i
+                            y2 = j
+                            score2 = score
+        @highLightRegion x1, y1
+        @highLightRegion x2, y2
+        for i in [0..4]
+            for j in [0..4]
+                @updateStyle i, j
+                    
+    doWin: =>
+        @win = true
+        @depth += 1
+        score = @getScore()
+        @scores.push score
+        @total += score
+        @initGame()
+    doLose: =>
+        @lose = true
+        @initGame()
+    
     
 gameDiv = document.getElementById 'game_div'
 game = new Game gameDiv
--- Rodeo/redeo.js
+++ Rodeo/redeo.js
@@ -5,17 +5,29 @@
   Game = (function() {
     function Game(gameDiv) {
       this.gameDiv = gameDiv;
+      this.doLose = __bind(this.doLose, this);
+      this.doWin = __bind(this.doWin, this);
+      this.highLight = __bind(this.highLight, this);
+      this.highLightRegion = __bind(this.highLightRegion, this);
       this.click = __bind(this.click, this);
+      this.minSearch = __bind(this.minSearch, this);
+      this.maxSearch = __bind(this.maxSearch, this);
+      this.getScore = __bind(this.getScore, this);
+      this.getRegionScore = __bind(this.getRegionScore, this);
+      this.updateStyle = __bind(this.updateStyle, this);
+      this.setHighLight = __bind(this.setHighLight, this);
       this.setBelong = __bind(this.setBelong, this);
       this.setNum = __bind(this.setNum, this);
       this.initGame = __bind(this.initGame, this);
       this.initBoard = __bind(this.initBoard, this);
       this.init = __bind(this.init, this);
+      this.stepx = [0, 1, 0, -1];
+      this.stepy = [1, 0, -1, 0];
     }
 
     Game.prototype.init = function() {
-      this.depth = 0;
-      this.score = [];
+      this.depth = 1;
+      this.scores = [];
       this.total = 0;
       this.initBoard();
       return this.initGame();
@@ -50,6 +62,10 @@
 
     Game.prototype.initGame = function() {
       var i, j, num, x, y, _i, _j, _k, _results;
+      this.turn = 1;
+      this.step = 0;
+      this.win = false;
+      this.lose = false;
       for (i = _i = 0; _i <= 4; i = ++_i) {
         for (j = _j = 0; _j <= 4; j = ++_j) {
           this.setNum(i, j, 0);
@@ -62,10 +78,7 @@
           var _l, _results1;
           _results1 = [];
           for (j = _l = -1; _l <= 2; j = ++_l) {
-            num = j;
-            if (j === 0) {
-              num = 1;
-            }
+            num = j === 0 ? 1 : j;
             _results1.push((function() {
               var _results2;
               _results2 = [];
@@ -89,9 +102,23 @@
     };
 
     Game.prototype.setNum = function(x, y, num) {
-      var text;
       this.board[x][y].num = num;
-      switch (num) {
+      return this.updateStyle(x, y);
+    };
+
+    Game.prototype.setBelong = function(x, y, belong) {
+      this.board[x][y].belong = belong;
+      return this.updateStyle(x, y);
+    };
+
+    Game.prototype.setHighLight = function(x, y, highLight) {
+      this.board[x][y].highLight = highLight;
+      return this.updateStyle(x, y);
+    };
+
+    Game.prototype.updateStyle = function(x, y) {
+      var className, text;
+      switch (this.board[x][y].num) {
         case -1:
           text = '-';
           break;
@@ -104,15 +131,232 @@
         case 2:
           text = '++';
       }
-      return this.board[x][y].div.innerText = text;
+      this.board[x][y].div.innerText = text;
+      switch (this.board[x][y].belong) {
+        case 0:
+          className = 'game_piece';
+          break;
+        case 1:
+          className = 'game_piece' + (this.board[x][y].highLight ? ' region1' : ' belong1');
+          break;
+        case 2:
+          className = 'game_piece' + (this.board[x][y].highLight ? ' region2' : ' belong2');
+      }
+      return this.board[x][y].div.className = className;
     };
 
-    Game.prototype.setBelong = function(x, y, belong) {
-      return this.board[x][y].belong = belong;
+    Game.prototype.getRegionScore = function(x, y) {
+      var k, score, tx, ty, _i;
+      score = this.board[x][y].num;
+      this.board[x][y].visit = true;
+      for (k = _i = 0; _i <= 3; k = ++_i) {
+        tx = x + this.stepx[k];
+        ty = y + this.stepy[k];
+        if (tx >= 0 && tx <= 4) {
+          if (ty >= 0 && ty <= 4) {
+            if (!this.board[tx][ty].visit) {
+              if (this.board[tx][ty].belong === this.board[x][y].belong) {
+                score += this.getRegionScore(tx, ty);
+              }
+            }
+          }
+        }
+      }
+      return score;
+    };
+
+    Game.prototype.getScore = function() {
+      var i, j, score, score1, score2, _i, _j, _k, _l;
+      for (i = _i = 0; _i <= 4; i = ++_i) {
+        for (j = _j = 0; _j <= 4; j = ++_j) {
+          this.board[i][j].visit = false;
+        }
+      }
+      score1 = 0;
+      score2 = 0;
+      for (i = _k = 0; _k <= 4; i = ++_k) {
+        for (j = _l = 0; _l <= 4; j = ++_l) {
+          if (!this.board[i][j].visit) {
+            score = this.getRegionScore(i, j);
+            if (this.board[i][j].belong === 1) {
+              if (score > score1) {
+                score1 = score;
+              }
+            } else if (this.board[i][j].belong === 2) {
+              if (score > score2) {
+                score2 = score;
+              }
+            }
+          }
+        }
+      }
+      return score1 - score2;
+    };
+
+    Game.prototype.maxSearch = function(beta, depth, num) {
+      var alpha, score, x, y, _i, _j;
+      if (depth === 0 || num === 25) {
+        return this.getScore();
+      }
+      alpha = -1e10;
+      for (x = _i = 0; _i <= 4; x = ++_i) {
+        for (y = _j = 0; _j <= 4; y = ++_j) {
+          if (this.board[x][y].belong === 0) {
+            this.board[x][y].belong = 1;
+            score = this.minSearch(alpha, depth - 1, num + 1);
+            this.board[x][y].belong = 0;
+            if (score > alpha) {
+              alpha = score;
+            }
+            if (alpha >= beta) {
+              return alpha;
+            }
+          }
+        }
+      }
+      return alpha;
+    };
+
+    Game.prototype.minSearch = function(alpha, depth, num) {
+      var beta, score, x, y, _i, _j;
+      if (depth === 0 || num === 25) {
+        return this.getScore();
+      }
+      beta = 1e10;
+      for (x = _i = 0; _i <= 4; x = ++_i) {
+        for (y = _j = 0; _j <= 4; y = ++_j) {
+          if (this.board[x][y].belong === 0) {
+            this.board[x][y].belong = 2;
+            score = this.maxSearch(alpha, depth - 1, num + 1);
+            this.board[x][y].belong = 0;
+            if (score < beta) {
+              beta = score;
+              if (num === this.step) {
+                this.nextx = x;
+                this.nexty = y;
+              }
+            }
+            if (alpha >= beta) {
+              return beta;
+            }
+          }
+        }
+      }
+      return beta;
     };
 
     Game.prototype.click = function(x, y) {
-      return console.log(x, y);
+      if (!this.win && !this.lose) {
+        if (this.board[x][y].belong === 0) {
+          this.setBelong(x, y, 1);
+          this.board[x][y].belong = 1;
+          this.step += 1;
+          if (this.step === 25) {
+            if (this.getScore() > 0) {
+              this.doWin();
+            } else {
+              this.doLose();
+            }
+          } else {
+            this.minSearch(-1e10, this.depth, this.step);
+            this.setBelong(this.nextx, this.nexty, 2);
+            this.step += 1;
+          }
+          return this.highLight();
+        }
+      }
+    };
+
+    Game.prototype.highLightRegion = function(x, y) {
+      var k, tx, ty, _i, _results;
+      this.board[x][y].highLight = true;
+      _results = [];
+      for (k = _i = 0; _i <= 3; k = ++_i) {
+        tx = x + this.stepx[k];
+        ty = y + this.stepy[k];
+        if (tx >= 0 && tx <= 4) {
+          if (ty >= 0 && ty <= 4) {
+            if (!this.board[tx][ty].highLight) {
+              if (this.board[tx][ty].belong === this.board[x][y].belong) {
+                _results.push(this.highLightRegion(tx, ty));
+              } else {
+                _results.push(void 0);
+              }
+            } else {
+              _results.push(void 0);
+            }
+          } else {
+            _results.push(void 0);
+          }
+        } else {
+          _results.push(void 0);
+        }
+      }
+      return _results;
+    };
+
+    Game.prototype.highLight = function() {
+      var i, j, score, score1, score2, x1, x2, y1, y2, _i, _j, _k, _l, _m, _results;
+      for (i = _i = 0; _i <= 4; i = ++_i) {
+        for (j = _j = 0; _j <= 4; j = ++_j) {
+          this.board[i][j].visit = false;
+          this.setHighLight(i, j, false);
+        }
+      }
+      if (this.step === 0) {
+        return;
+      }
+      score1 = 0;
+      score2 = 0;
+      for (i = _k = 0; _k <= 4; i = ++_k) {
+        for (j = _l = 0; _l <= 4; j = ++_l) {
+          if (!this.board[i][j].visit) {
+            score = this.getRegionScore(i, j);
+            if (this.board[i][j].belong === 1) {
+              if (score > score1) {
+                score1 = score;
+                x1 = i;
+                y1 = j;
+              }
+            } else if (this.board[i][j].belong === 2) {
+              if (score > score2) {
+                x2 = i;
+                y2 = j;
+                score2 = score;
+              }
+            }
+          }
+        }
+      }
+      this.highLightRegion(x1, y1);
+      this.highLightRegion(x2, y2);
+      _results = [];
+      for (i = _m = 0; _m <= 4; i = ++_m) {
+        _results.push((function() {
+          var _n, _results1;
+          _results1 = [];
+          for (j = _n = 0; _n <= 4; j = ++_n) {
+            _results1.push(this.updateStyle(i, j));
+          }
+          return _results1;
+        }).call(this));
+      }
+      return _results;
+    };
+
+    Game.prototype.doWin = function() {
+      var score;
+      this.win = true;
+      this.depth += 1;
+      score = this.getScore();
+      this.scores.push(score);
+      this.total += score;
+      return this.initGame();
+    };
+
+    Game.prototype.doLose = function() {
+      this.lose = true;
+      return this.initGame();
     };
 
     return Game;
