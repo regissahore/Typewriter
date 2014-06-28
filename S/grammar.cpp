f98ed45b912e68b80763f042964815540c78cc38
CyberZHG <CyberZHG@gmail.com>
2014-06-28 14:49:40 +0800

    Quaternions for expression.
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- S/grammar.cpp
+++ S/grammar.cpp
@@ -1,21 +1,87 @@
+#include <map>
+#include <cstdio>
 #include <iostream>
 #include "grammar.h"
 using namespace std;
+
+struct SymbolNode {
+    string symbol;
+    int value;
+};
+
+int constIndex;
+int tempSymbolIndex;
+vector<SymbolNode> symbolTable;
+map<string, int> symbolMap;
+int labelIndex;
+vector<SymbolNode> labelTable;
+
+void addSymbol(string &symbol) {
+    symbolMap[symbol] = symbolTable.size();
+    symbolTable.push_back({symbol, 0});
+}
+
+bool isSymbolExist(string &symbol) {
+    return symbolMap.find(symbol) != symbolMap.end();
+}
+
+int getSymbol(string &symbol) {
+    return symbolMap[symbol];
+}
+
+string addTempSymbol() {
+    char num[12];
+    sprintf(num, "%d", ++tempSymbolIndex);
+    string symbol = string("$") + num;
+    if (!isSymbolExist(symbol)) {
+        addSymbol(symbol);
+    }
+    return symbol;
+}
+
+void addLabel(int pos) {
+    char num[12];
+    sprintf(num, "%d", ++labelIndex);
+    string label = string("#") + num;
+    labelTable.push_back({label, pos});
+}
+
+string addConst(int value) {
+    char num[12];
+    sprintf(num, "%d", ++constIndex);
+    string constant = string("%") + num;
+    addSymbol(constant);
+    symbolTable[getSymbol(constant)].value = value;
+    return constant;
+}
+
+void addQuaternion(vector<Quaternion> &quats, string op, string symbol1, string symbol2, string symbol3) {
+    int addr1 = 0, addr2 = 0, addr3 = 0;
+    if (symbol1.size() > 0) {
+        addr1 = getSymbol(symbol1);
+    }
+    if (symbol2.size() > 0) {
+        addr2 = getSymbol(symbol2);
+    }
+    if (symbol3.size() > 0) {
+        addr3 = getSymbol(symbol3);
+    }
+    quats.push_back({op, symbol1, symbol2, symbol3, addr1, addr2, addr3});
+}
 
 void P(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
 void D(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
 void S(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
 void L(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
 void L_(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
-void B(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
-void T_(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
-void F_(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
-void E(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
-void T(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
-void F(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
+string B(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
+string T_(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
+string F_(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
+string E(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
+string T(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
+string F(vector<Quaternion> &quats, vector<Token> &tokens, int &index);
 
 void P(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
-    cout << index << "P" << tokens[index].type << " " << tokens[index].mnemonic << " " << tokens[index].token << endl;
     if (tokens[index].type == TYPE_L_BRACKET) {
         ++index;
         D(quats, tokens, index);
@@ -26,10 +92,10 @@ void P(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
 }
 
 void D(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
-    cout << index << "D" << tokens[index].type << " " << tokens[index].mnemonic << " " << tokens[index].token << endl;
     while (tokens[index].type == TYPE_INT) {
         ++index;
         if (tokens[index].type == TYPE_ID) {
+            addSymbol(tokens[index].token);
             ++index;
             if (tokens[index].type == TYPE_SEMICOLON) {
                 ++index;
@@ -43,7 +109,6 @@ void D(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
 }
 
 void S(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
-    cout << index << "S" << tokens[index].type << " " << tokens[index].mnemonic << " " << tokens[index].token << endl;
     if (tokens[index].type == TYPE_IF) {
         ++index;
         if (tokens[index].type == TYPE_L_PARENTHESE) {
@@ -95,10 +160,12 @@ void S(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
             cerr << "The brackets are mismatched." << endl;
         }
     } else if (tokens[index].type == TYPE_ID) {
+        string symbol1 = tokens[index].token;
         ++index;
         if (tokens[index].type == TYPE_ASSIGN) {
             ++index;
-            E(quats, tokens, index);
+            string symbol2 = E(quats, tokens, index);
+            addQuaternion(quats, "=", symbol2, "", symbol1);
         } else {
             cerr << "Should be assignment." << endl;
         }
@@ -108,97 +175,150 @@ void S(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
 }
 
 void L(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
-    cout << index << "L" << tokens[index].type << " " << tokens[index].mnemonic << " " << tokens[index].token << endl;
     S(quats, tokens, index);
     L_(quats, tokens, index);
 }
 
 void L_(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
-    cout << index << "L'" << tokens[index].type << " " << tokens[index].mnemonic << " " << tokens[index].token << endl;
     if (tokens[index].type == TYPE_SEMICOLON) {
         ++index;
         L(quats, tokens, index);
     }
 }
 
-void B(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
-    cout << index << "B" << tokens[index].type << " " << tokens[index].mnemonic << " " << tokens[index].token << endl;
-    T_(quats, tokens, index);
+string B(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
+    string symbol1 = T_(quats, tokens, index);
     if (tokens[index].type == TYPE_OR) {
         ++index;
-        T_(quats, tokens, index);
+        string symbol2 = T_(quats, tokens, index);
+        string symbol3 = addTempSymbol();
+        addQuaternion(quats, "|", symbol1, symbol2, symbol3);
+        return symbol3;
     }
+    return symbol1;
 }
 
-void T_(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
-    cout << index << "T'" << tokens[index].type << " " << tokens[index].mnemonic << " " << tokens[index].token << endl;
-    F_(quats, tokens, index);
+string T_(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
+    string symbol1 = F_(quats, tokens, index);
     if (tokens[index].type == TYPE_AND) {
         ++index;
-        F_(quats, tokens, index);
+        string symbol2 = F_(quats, tokens, index);
+        string symbol3 = addTempSymbol();
+        addQuaternion(quats, "&", symbol1, symbol2, symbol3);
+        return symbol3;
     }
+    return symbol1;
 }
 
-void F_(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
-    cout << index << "F'" << tokens[index].type << " " << tokens[index].mnemonic << " " << tokens[index].token << endl;
-    F(quats, tokens, index);
+string F_(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
+    string symbol1 = F(quats, tokens, index);
     if (tokens[index].type == TYPE_RELOP) {
+        string relop = tokens[index].token;
         ++index;
-        F(quats, tokens, index);
+        string symbol2 = F(quats, tokens, index);
+        string symbol3 = addTempSymbol();
+        addQuaternion(quats, relop, symbol1, symbol2, symbol3);
+        return symbol3;
     }
+    return symbol1;
 }
 
-void E(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
-    cout << index << "E" << tokens[index].type << " " << tokens[index].mnemonic << " " << tokens[index].token << endl;
-    T(quats, tokens, index);
-    if (tokens[index].type == TYPE_ADD) {
+string E(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
+    string symbol1 = T(quats, tokens, index);
+    if (tokens[index].type == TYPE_ADD || tokens[index].type == TYPE_SUB) {
+        string op = tokens[index].mnemonic;
         ++index;
-        T(quats, tokens, index);
-    } else if (tokens[index].type == TYPE_SUB) {
-        ++index;
-        T(quats, tokens, index);
+        string symbol2 = T(quats, tokens, index);
+        string symbol3 = addTempSymbol();
+        addQuaternion(quats, op, symbol1, symbol2, symbol3);
+        return symbol3;
     }
+    return symbol1;
 }
 
-void T(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
-    cout << index << "T" << tokens[index].type << " " << tokens[index].mnemonic << " " << tokens[index].token << endl;
-    F(quats, tokens, index);
-    if (tokens[index].type == TYPE_MUL) {
+string T(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
+    string symbol1 = F(quats, tokens, index);
+    if (tokens[index].type == TYPE_MUL || tokens[index].type == TYPE_DIV) {
+        string op = tokens[index].mnemonic;
         ++index;
-        F(quats, tokens, index);
-    } else if (tokens[index].type == TYPE_DIV) {
-        ++index;
-        F(quats, tokens, index);
+        string symbol2 = F(quats, tokens, index);
+        string symbol3 = addTempSymbol();
+        addQuaternion(quats, op, symbol1, symbol2, symbol3);
+        return symbol3;
     }
+    return symbol1;
 }
 
-void F(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
-    cout << index << "F" << tokens[index].type << " " << tokens[index].mnemonic << " " << tokens[index].token << endl;
+string F(vector<Quaternion> &quats, vector<Token> &tokens, int &index) {
     if (tokens[index].type == TYPE_L_PARENTHESE) {
         ++index;
-        E(quats, tokens, index);
+        string symbol = E(quats, tokens, index);
         if (tokens[index].type == TYPE_R_PARENTHESE) {
             ++index;
         } else {
             cerr << "The parentheses are mismatched." << endl;
         }
-    } else if (tokens[index].type == TYPE_CONST) {
-        ++index;
-    } else if (tokens[index].type == TYPE_ID) {
-        ++index;
-    } else {
-        cerr << "Expression should surrounded by parentheses." << endl;
+        return symbol;
     }
+    if (tokens[index].type == TYPE_CONST) {
+        int constant;
+        sscanf(tokens[index].token.c_str(), "%d", &constant);
+        string symbol = addConst(constant);
+        ++index;
+        return symbol;
+    }
+    if (tokens[index].type == TYPE_ID) {
+        string symbol = tokens[index].token;
+        if (!isSymbolExist(symbol)) {
+            cerr << symbol << " is not defined." << endl;
+        }
+        ++index;
+        return symbol;
+    }
+    cerr << "Expression should surrounded by parentheses." << endl;
+    return "";
 }
 
 vector<Quaternion> getQuaternions(vector<Token> &tokens) {
     int index = 0;
     vector<Quaternion> quats;
+    constIndex = 0;
+    tempSymbolIndex = 0;
+    symbolTable.clear();
+    symbolMap.clear();
+    labelIndex = 0;
+    labelTable.clear();
     P(quats, tokens, index);
     return quats;
 }
 
 #ifdef DEBUG
 void printQuaternions(vector<Quaternion> &quaternions) {
+    cout << "Symbol Table: " << endl;
+    for (size_t i = 0; i < symbolTable.size(); ++i) {
+        cout << i << "\t" << symbolTable[i].symbol << "\t";
+        cout << symbolTable[i].value << endl;
+    }
+    cout << "Label Table: " << endl;
+    for (size_t i = 0; i < labelTable.size(); ++i) {
+        cout << i << "\t" << labelTable[i].symbol << "\t";
+        cout << labelTable[i].value << endl;
+    }
+    cout << "Quaternions with symbol: " << endl;
+    for (size_t i = 0; i < quaternions.size(); ++i) {
+        Quaternion quat = quaternions[i];
+        cout << i << "\t" << quat.op << "\t";
+        cout << quat.symbol1 << "\t";
+        cout << quat.symbol2 << "\t";
+        cout << quat.symbol3 << endl;
+    }
+    cout << "Quaternions with address: " << endl;
+    for (size_t i = 0; i < quaternions.size(); ++i) {
+        Quaternion quat = quaternions[i];
+        cout << i << "\t" << quat.op << "\t";
+        cout << quat.addr1 << "\t";
+        cout << quat.addr2 << "\t";
+        cout << quat.addr3 << endl;
+    }
 }
 #endif // DEBUG
--- S/grammar.h
+++ S/grammar.h
@@ -8,9 +8,12 @@
 struct Quaternion
 {
     std::string op;
-    int src1;
-    int src2;
-    int dst;
+    std::string symbol1;
+    std::string symbol2;
+    std::string symbol3;
+    int addr1;
+    int addr2;
+    int addr3;
 };
 
 std::vector<Quaternion> getQuaternions(std::vector<Token> &tokens);
